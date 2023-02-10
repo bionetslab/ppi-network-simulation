ac5b021fe02da1730bddd21800138f33227bf614
marta <ieo5924@marta.garr.cloud.ct>
2023-02-10 10:45:43 +0000

    clean script_simulation.py

--- script_simulation.py
+++ script_simulation.py
@@ -43,35 +43,6 @@ for p in par:
             json.dump(data, outfile, indent = 4)
 
 print('parameters files done!')
-#----------------------------------------------------------------------------
-# if os.path.exists('output_results') == False:
-#   os.mkdir('output_results')
-# 
-# method = ['AP-MP','Y2H']
-# nsg = 50
-# 
-# for m in method:
-#   dir_parameters = 'parameter_settings/all_param_combinations/'+ m +'/'
-#   files = os.listdir(dir_parameters)
-#   
-#   for f in files:
-#     parameters = ppinetsim.Parameters('parameter_settings/all_param_combinations/'+m+'/'+f)
-#     
-#     if os.path.exists('output_results/'+str(parameters.test_method)) == False:
-#         os.mkdir('output_results/'+str(parameters.test_method))
-#     d = 'output_results/'+str(parameters.test_method)+'/accTh' + str(parameters.acceptance_threshold).replace('.','') + '_FPR'+ str(parameters.false_positive_rate).replace('.','') + '_FNR'+ str(parameters.false_negative_rate).replace('.','')
-#     if(os.path.exists(d)) == False:
-#       os.mkdir(d)
-#       
-#     likelihood_at_k, all_results = ppinetsim.estimate_likelihood(parameters, num_simulations_per_generator=nsg)
-#     likelihood_at_k.to_csv(d+'/likelihood_'+parameters.test_method + '_'+ 'accTh' + str(parameters.acceptance_threshold).replace('.','') + '_FPR'+ str(parameters.false_positive_rate).replace('.','') + '_FNR'+ str(parameters.false_negative_rate).replace('.','') +'.csv', index = False)
-#     #save results in json
-#     results4json=[]
-#     for result in all_results:
-#       temp=list(result)
-#       temp[2]=[list(temp[2][0]),list(temp[2][1])]
-#       results4json.append(tuple(temp))
-#       json.dump(results4json, codecs.open(d+'/all_results.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
 
 
 ##à#### version with Parallel #####
@@ -117,14 +88,14 @@ method = ['AP-MS','Y2H']
 nsg = 50
 jobs = 8
 
-method = ['Y2H']
+method = ['AP-MS']
 start_time = time.time()
 for m in method:
   dir_parameters = 'parameter_settings/all_param_combinations/'+ m +'/'
   print(m)
   files = os.listdir(dir_parameters)
   #files = files[24:32]
-  files = ['params_Y2H_accTh00_FPR00125_FNR00.json','params_Y2H_accTh00_FPR00125_FNR01.json','params_Y2H_accTh00_FPR00125_FNR02.json','params_Y2H_accTh00_FPR00125_FNR03.json','params_Y2H_accTh00_FPR00125_FNR04.json','params_Y2H_accTh00_FPR0025_FNR00.json','params_Y2H_accTh00_FPR0025_FNR01.json','params_Y2H_accTh00_FPR0025_FNR02.json','params_Y2H_accTh00_FPR0025_FNR03.json','params_Y2H_accTh00_FPR0025_FNR04.json','params_Y2H_accTh00_FPR005_FNR01.json','params_Y2H_accTh00_FPR005_FNR02.json','params_Y2H_accTh00_FPR005_FNR04.json']
+  files = ['params_AP-MS_accTh05_FPR00125_FNR00.json','params_AP-MS_accTh05_FPR00125_FNR01.json','params_AP-MS_accTh05_FPR00125_FNR02.json','params_AP-MS_accTh05_FPR00125_FNR03.json','params_AP-MS_accTh05_FPR00125_FNR04.json','params_AP-MS_accTh05_FPR0025_FNR00.json','params_AP-MS_accTh05_FPR0025_FNR01.json','params_AP-MS_accTh05_FPR0025_FNR02.json','params_AP-MS_accTh05_FPR0025_FNR03.json','params_AP-MS_accTh05_FPR0025_FNR04.json','params_AP-MS_accTh05_FPR005_FNR00.json','params_AP-MS_accTh05_FPR005_FNR01.json','params_AP-MS_accTh05_FPR005_FNR02.json','params_AP-MS_accTh05_FPR005_FNR03.json','params_AP-MS_accTh05_FPR005_FNR04.json','params_AP-MS_accTh05_FPR02_FNR03.json']
   print(files)
   Parallel(n_jobs = jobs)(delayed(simulation_forParallel)(m,f,nsg) for f in files)
 print(time.time() - start_time)
