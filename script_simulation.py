a6cc709598217d6a2e45c5583cd986878a9baeca
marta <ieo5924@marta.garr.cloud.ct>
2023-03-08 13:24:24 +0000

    clean script_simulation.py

--- script_simulation.py
+++ script_simulation.py
@@ -16,7 +16,6 @@ import codecs
 
 ##### create json input files #######
 
-#seq = np.arange(0,0.45,0.05)
 seq_FP = [0.0,0.003125,0.00625,0.0125,0.025,0.05,0.1,0.2,0.3,0.4]
 seq_FN = [0.0,0.1,0.2,0.3,0.4]
 
@@ -45,7 +44,7 @@ for p in par:
 print('parameters files done!')
 
 
-##à#### version with Parallel #####
+###### version with Parallel #####
 
 def simulation_forParallel(m,f,nsg):
   """Runs the simulation for several combinations of parameters.
@@ -88,13 +87,11 @@ method = ['AP-MS','Y2H']
 nsg = 50
 jobs = 8
 
-method = ['AP-MS']
 start_time = time.time()
 for m in method:
   dir_parameters = 'parameter_settings/all_param_combinations/'+ m +'/'
   print(m)
   files = os.listdir(dir_parameters)
-  files = ['params_AP-MS_accTh05_FPR0003125_FNR00.json','params_AP-MS_accTh05_FPR0003125_FNR01.json','params_AP-MS_accTh05_FPR0003125_FNR02.json','params_AP-MS_accTh05_FPR0003125_FNR03.json','params_AP-MS_accTh05_FPR0003125_FNR04.json','params_AP-MS_accTh05_FPR000625_FNR00.json','params_AP-MS_accTh05_FPR000625_FNR01.json','params_AP-MS_accTh05_FPR000625_FNR02.json','params_AP-MS_accTh05_FPR000625_FNR03.json','params_AP-MS_accTh05_FPR000625_FNR04.json']
   print(files)
   Parallel(n_jobs = jobs)(delayed(simulation_forParallel)(m,f,nsg) for f in files)
 print(time.time() - start_time)
