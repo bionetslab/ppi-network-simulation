3dfa40fd8ffe849e34712987dacf80a2cb05f177
marta <ieo5924@marta.garr.cloud.ct>
2023-03-08 13:30:17 +0000

    Deleted the file from the git repository

--- main.py
+++
@@ -1,8 +0,0 @@
-import powerlaw as pl
-import os
-
-dir_parameters = 'parameter_settings'
-files = os.listdir(dir_parameters)
-n = 1
-dir_output = '.'
-pl.run_iterations_withParallel(files,n,dir_parameters,100,1,dir_output,'observed_network',1)
